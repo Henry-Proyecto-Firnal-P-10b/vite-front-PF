bdd3c6780b10288b32ad4bad26f54313f3727b1e
felipegavilan <felipegaviilan@gmail.com>
2023-05-18 14:58:08 -0300

    corrección ruta

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Authentication from "./routes/authentication/authentication";
 import {
   createUserDocumentFromAuth,
   getCategories,
+  getOrdersAdmin,
   getProducts,
   onAuthStateChangedListener,
 } from "./utils/firebase/firebaseClient";
@@ -26,9 +27,10 @@ import {
 } from "./features/products/productSlice";
 import { getCategoriesAction } from "./features/categories/categoriesSlice";
 import SignUp from "./routes/authentication/signUp";
+
 // import User from "./components/panel-admin/user/User";
 import UserProfile from "./routes/userProfile/UserProfile";
-import AboutUs from "./routes/AboutUs/AboutUs";
+import AboutUs from "./routes/aboutUs/aboutUs";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -64,6 +66,19 @@ const App = () => {
     });
     let category = await getCategories();
     dispatch(getCategoriesAction(category));
+    getOrdersAdmin(a=>{
+      const orders = [];
+      a.forEach((element) => {
+        const id = element.id;
+        const data = element.data();
+        orders.push({
+          id,
+          ...data,
+        });
+      });
+      dispatch(ordersAction(orders))
+      
+    })
   };
 
   return (
@@ -75,7 +90,7 @@ const App = () => {
         <Route path="shop/*" element={<Shop />} />
         <Route path="detail/:id" element={<DetailProduct />} />
         <Route path="perfil" element={<UserProfile/>} />
-        <Route path="aboutUs" element={<AboutUs/>} />
+        <Route path="about-us" element={<AboutUs/>} />
       </Route>
       {userData?.admin && (
         <Route
